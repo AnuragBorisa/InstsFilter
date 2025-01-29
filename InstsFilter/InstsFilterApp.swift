8209260b84baf2da13b9571f96aa0aba54bf0cfb
Anurag Borisa <anuragborisa@gmail.com>
2025-01-29 18:13:12 +0530

    Initial Commit

---
+++ InstsFilter/InstsFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstsFilterApp.swift
+//  InstsFilter
+//
+//  Created by Anurag on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct InstsFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstsFilterTests/InstsFilterTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstsFilterTests.swift
+//  InstsFilterTests
+//
+//  Created by Anurag on 29/01/25.
+//
+
+import Testing
+@testable import InstsFilter
+
+struct InstsFilterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
