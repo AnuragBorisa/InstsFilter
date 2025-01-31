43cb707a537bcf8d13e1f44ad61c3c3e3dc38e97
Anurag Borisa <anuragborisa@gmail.com>
2025-02-01 00:33:33 +0530

    added more filters

--- InstsFilter/ContentView.swift
+++ InstsFilter/ContentView.swift
@@ -9,14 +9,26 @@ import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
 import PhotosUI
+import StoreKit
+
 struct ContentView: View {
+    @AppStorage("filterCount") var filterCount = 0;
+    @Environment(\.requestReview) var requestReview
     @State private var selectedItem : PhotosPickerItem?
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var radius = 0.5
+    @State private var scale = 0.5
     @State private var currentFilter : CIFilter = CIFilter.sepiaTone()
     @State private var showingFilter = false;
     @State private var beginImage : CIImage?
    
+    var disableSlider:Bool {
+        if (processedImage != nil) {
+            return false;
+        }
+        return true;
+    }
     
     let context = CIContext()
     
@@ -24,9 +36,16 @@ struct ContentView: View {
         showingFilter = true
     }
     
+    @MainActor
     func setFilter(_ filter:CIFilter){
         currentFilter = filter
         loadImage()
+        
+        filterCount += 1
+        
+        if filterCount >= 20 {
+            requestReview()
+        }
     }
     
     func loadImage(){
@@ -48,8 +67,8 @@ struct ContentView: View {
        
 
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(radius * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(scale * 10, forKey: kCIInputScaleKey) }
         
       
         
@@ -77,19 +96,40 @@ struct ContentView: View {
                 .onChange(of: selectedItem,loadImage)
                 Spacer()
                 
-                HStack{
-                    Text("Intensity")
-                    Slider(value:$filterIntensity)
-                        .onChange(of: filterIntensity, applyProcessing)
+                VStack{
+                    HStack{
+                        Text("Intensity")
+                        Slider(value:$filterIntensity)
+                            .onChange(of: filterIntensity, applyProcessing)
+                    }
+                    
+                    HStack{
+                        Text("Radius")
+                        Slider(value:$radius)
+                            .onChange(of: radius, applyProcessing)
+                    }
+                    
+                    HStack{
+                        Text("Scale")
+                        Slider(value:$scale)
+                            .onChange(of: scale, applyProcessing)
+                    }
+    
                 }
                 .padding(.vertical)
+                .disabled(disableSlider)
                 
                 HStack{
                     Button("Change Filter",action: changeFilter)
+                        .disabled(disableSlider)
 //                        change the filter
                     
                     Spacer()
 //                    share the picture
+                    
+                    if let processedImage {
+                        ShareLink(item: processedImage,preview: SharePreview("Instafilter image",image: processedImage))
+                    }
                 }
             }
             .padding([.horizontal,.bottom])
@@ -102,7 +142,9 @@ struct ContentView: View {
                 Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
                 Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
                 Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Box Blur"){setFilter(CIFilter.boxBlur())}
                 Button("Cancel", role: .cancel) { }
+                
             }
             
         }
